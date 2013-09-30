77e0f5936b8cdd6c1342c96696c43e1485a44ed5
LoucIsZbib <massluc@gmail.com>
2013-09-30 11:47:06 +0200

    Update scn.sh
    Mise en place du multilinguisme
    Reste à traduire en anglais

--- scn.sh
+++ scn.sh
@@ -29,7 +29,8 @@ user=changeme
 password=changeme
 version=changeme
 client=subconsolnic
-
+#lang: fr, en
+lang=fr
 
 ## CHECKING TOOLS
 #Checking wget
@@ -57,39 +58,126 @@ exit 0
 fi
 
 ## FUNCTION REPO ##
+#Interface Utilisateur
+case $lang in
+fr|FR)
+UI_startMenu="\
+Bienvenue sur Subconsolnic !\n\
+Let's play !\n\
+----------------------------\n\
+| S-U-B-C-O-N-S-O-L-N-I-C  |\n\
+|__________________________|\n\
+|                          |\n\
+|   Touches Disponibles    |\n\
+| 1 -> Recherche Albums    |\n\
+| 2 -> Parcourir dossiers  |\n\
+| 3 -> Entrer ID           |\n\
+| 4 -> controler mplayer   |\n\
+| 5 -> Quitter             |\n\
+|__________________________|"
+
+UI_searchMenu="\
+################################\n\
+#  Choix possibles :           #\n\
+# v -> voir contenu de l'album #\n\
+# p -> jouer l'album	       #\n\
+# q -> retour menu principal   #\n\
+################################"
+
+UI_infosmenu="\
+############################\n\
+#     Choix disponibles    #\n\
+#	                   #\n\
+# i -> infos sur dossier   #\n\
+# p -> jouer album         #\n\
+# q -> menu principal      #\n\
+############################"
+
+UI_controlemplayermenu="\
+#########################\n\
+#   CONTROLE MPLAYER    #\n\
+# P : Pause             #\n\
+# N : Chanson Suivante  #\n\
+# B : Chanson precedente#\n\
+#                       #\n\
+# E : Avancer morceau   #\n\
+# R : Reculer morceau   #\n\
+#                       #\n\
+# S : Stop player       #\n\
+# Q : Revenir au menu   #\n\
+#########################"
+
+# controlemplayer function
+UI_nowListening="Vous ecoutez actuellement :"
+UI_pause="\
+=====PAUSE=====\n\
+Reprendre ? [O]"
+UI_unpause="=====Reprise===="
+UI_nextSong="Chanson suivante..."
+UI_prevSong="Chanson precedente..."
+UI_fastForward="Avance Rapide"
+UI_fastBackward="Retour arriere"
+UI_stopPlayer="Arret du Player"
+UI_purgingPlayerPipe="Suppression du controleur mplayer.pipe"
+UI_purginPlaylist="Suppression de la playlist"
+UI_purgingLogs="Suppression des logs"
+
+# Jukebox function
+UI_jukeboxLoading="Chargement du lecteur..."
+UI_songLoading="Chargement de la chanson"
+UI_3dot="..."
+
+# serach function
+UI_searchtitle="Recherche d'albums par artiste/piste/albums"
+UI_searchInput="Tapez votre recherche"
+UI_searching="Je recherche tout de suite :"
+
+# infosmenu function
+UI_fileInput="Taper l'ID (chiffre sur la dernière colonne) pour plus d'informations sur le dossier"
+
+# startmenu function
+UI_quit="Good luck without sound !"
+
+# global 
+UI_albumIDinput="Taper l'ID de l'album (dernière colonne)"
+UI_wrongKey="Saisie erronée, veuillez recommencer"
+
+;;
+en|EN)
+
+;;
+*)
+	echo "wrong lang parameter"
+	echo "paramètre lang erroné"
+	exit 2
+	;;
+esac
+
 #Fonction Recherche
 function recherche {
     boucleRecherche=true
     while $boucleRecherche
     do
-	echo "Recherche d'albums par artiste/piste/albums"
-        echo -n "Tapez votre recherche"
+	echo $UI_searchtitle
+        echo -n $UI_searchInput
 	read search
-	echo "Je recherche tout de suite :" $search
+	echo $UI_searching $search
         #Envoi de la requete via l'api
         wget -q "$server/rest/search2.view?u=$user&p=$password&v=$version&c=$client&songCount=0&query=$search&songCount=0" -O - | xmlstarlet sel -N n=http://subsonic.org/restapi -t -m "//n:album" -v "concat(@title, '     ', @album, '      ', @artist, '    ', @id)" -n | cat -n | sed -e '$d'
 
-
-
-	echo "################################"
-	echo "#  Choix possibles :           #"
-	echo "# v -> voir contenu de l'album #"
-	echo "# p -> jouer l'album	     #"
-	#echo "# autre touche -> retour       #"
-	echo "# q -> retour menu principal   #"
-	echo "################################"
+	echo $UI_searchMenu
 	read -n 1 choice2
 
 	case $choice2 in
 		v|V)   # voir le contenu de l'album
-			echo -n "Taper l'ID (derniere colonne) de l'album"
+			echo -n $UI_albumIDinput
 			read id
 			getMusicDirectory			
 			# on reste dans la boucle recherche
 			;;	
 
 		p|P)    # jouer l'album
-			echo -n "Taper l'ID (dernière colonne) de l'album"
+			echo -n $UI_albumIDinput
 			read id
 			jukebox
 			# une fois la lecture/jukebox finie, on revient au menuPrincipal
@@ -101,7 +189,7 @@ function recherche {
 			;;
 
 		*) 	# saisie erronée
-			echo "Saisie erronée, veuillez recommencer"
+			echo $UI_wrongKey
 			# on reste dans la boucle recherche
 			;;
 	esac
@@ -120,27 +208,21 @@ function infosmenus {
     boucleInfosmenus=true
     while $boucleInfosmenus
     do
-	echo "############################"
-	echo "#     Choix disponibles    #"
-	echo "#	                         #"
-	echo "# i -> infos sur dossier   #"
-	echo "# p -> jouer album         #"
-	echo "# q -> menu principal      #"
-	echo "############################"
+	echo $UI_infosmenu
 	read -n 1 chapichapo
 	
 	case $chapichapo in 
 	i)	# infos sur le dossier
-		echo -n "Taper l'ID (chiffre sur la dernière colonne) pour plus d'informations sur le dossier"
+		echo -n $UI_fileInput
         	read id
         	if [ $(echo $id | grep -v [a-Z] | wc -l) -eq 0 ]; then
-        		echo "Saisie erronée, veuillez recommencer"
+        		echo $UI_wrongKey
         	else
         		getMusicDirectory
 		fi
 		;;
 	p)	# jouer l'album
-		echo -n "Taper l'ID de l'album (dernière colonne)"
+		echo -n $UI_albumIDinput
 		read id
 		jukebox
 		# une fois la lecture/jukebox finie, retour au menuPrincipal
@@ -150,7 +232,7 @@ function infosmenus {
 		boucleInfosmenus=false
 		;;
 			
-	*)	echo "Saisie erronée, veuillez recommencer"
+	*)	echo $UI_wrongKey
 		;;
 	esac
     done
@@ -162,7 +244,7 @@ function jukebox {
 	#Creation du fichier de controle mplayer en slave
 	#si il n'existe pas
 
-	echo "Chargement du lecteur..."
+	echo $UI_jukeboxLoading
 	fifo=`ls /tmp/ | grep mplayer.pipe`
 	if [ -z $fifo ]; then
 	mkfifo /tmp/mplayer.pipe
@@ -192,11 +274,11 @@ function jukebox {
 		echo "http://$server/rest/download.view?u=$user&p=$password&v=$version&c=$client&id=$line" >> /tmp/playlist
 	done
 	
-	echo "Chargement de la chanson"
+	echo $UI_songLoading
 		
 	nohup mplayer -slave -input file=/tmp/mplayer.pipe -nocache -prefer-ipv4 -playlist /tmp/playlist > /tmp/scnlog 2>/dev/null &
 	
-	echo "..."
+	echo $UI_3dot
 	#sleep permettant d'attendre le lancement de mplayer avant 
 	#le début des tests de présence du processus
 	sleep 5
@@ -215,67 +297,55 @@ function controlemplayer {
 	wget -q "$server/rest/getSong.view?u=$user&p=$password&v=$version&c=$client&id=$recuperationid" -O - | xmlstarlet sel -N n=http://subsonic.org/restapi -t -m "//n:song" -v "concat('Titre : ', @title)" -n -o " " -n -v "concat('Artiste : ', @artist)" -n -o " " -n -v "concat('Album : ',@album)" > /tmp/lolog
 	
 	clear
-	echo " Vous ecoutez actuellement :"
+	echo $UI_nowListening
 	echo ""
 	cat /tmp/lolog
 	echo ""
 	echo ""
-        echo "#########################"
-        echo "#   CONTROLE MPLAYER    #"
-        echo "# P : Pause             #"
-        echo "# N : Chanson Suivante  #"
-        echo "# B : Chanson precedente#"
-	echo "#                       #"
-	echo "# E : Avancer morceau   #"
-	echo "# R : Reculer morceau   #"
-	echo "#                       #"
-	echo "# S : Stop player       #"
-	echo "# Q : Revenir au menu   #"
-        echo "#########################"
+        echo $UI_controlemplayermenu
         read -t 1 -n 1 controle
         
 	case $controle in 
 		p|P)	# PAUSE
 			echo "pause" > /tmp/mplayer.pipe
-			echo "=====PAUSE====="
-			echo "Reprendre ? [O]"
+			echo $UI_pause
 			read -p "==============="
 		 	
-			echo "=====Reprise===="
+			echo $UI_unpause
 			echo "pause" > /tmp/mplayer.pipe
 			sleep 1
 			;;
 			
 		n|N)	# chanson suivante
 			echo "pt_step 1" > /tmp/mplayer.pipe
-			echo "Chanson suivante..."
+			echo $UI_nextSong
 			sleep 5
 			;;
 
 		b|B)	# chanson précédente
 			echo "pt_step -1" > /tmp/mplayer.pipe
-			echo "Chanson precedente..."
+			echo $UI_prevSong
 			sleep 5
 			;;
 		
 		e|E)	# avance rapide
-			echo "Avance Rapide"
+			echo $UI_fastForward
 			echo "seek +20" > /tmp/mplayer.pipe
 			;;
 			
 		r|R)	# retour rapide
-			echo "Retour arriere"
+			echo $UI_fastBackward
 			echo "seek -20" > /tmp/mplayer.pipe
 			;;
 			
 		s|S)	# stop et retour menuprincipal
-			echo "Arret du Player"
+			echo $UI_stopPlayer
 			echo "stop" > /tmp/mplayer.pipe
-			echo "Suppression du controleur mplayer.pipe"
+			echo $UI_purgingPlayerPipe
 			rm /tmp/mplayer.pipe
-			echo "Suppression de la playlist"
+			echo $UI_purginPlaylist
 			rm /tmp/playlist
-			echo "Suppression des logs"
+			echo $UI_purgingLogs
 			rm /tmp/scnlog
 			rm /tmp/lolog
 			# on quitte et on revient au menu principal
@@ -297,19 +367,7 @@ function controlemplayer {
 
 function startmenu {
 #Start menu
-	echo "Bienvenue sur Subconsolnic !"
-	echo "Let's play !"
-	echo " --------------------------"
-	echo "| S-U-B-C-O-N-S-O-L-N-I-C  |"
-	echo "|__________________________|"
-	echo "|                          |"
-	echo "|   Touches Disponibles    |"
-	echo "| 1 -> Recherche Albums    |"
-	echo "| 2 -> Parcourir dossiers  |"
-	echo "| 3 -> Entrer ID           |"
-	echo "| 4 -> controler mplayer   |"
-	echo "| 5 -> Quitter             |"
-	echo "|__________________________|"
+	echo $UI_startMenu
 	read -n 1 choice
 	
 	
@@ -325,7 +383,7 @@ function startmenu {
 		;; 
 	
 	3)	# jouer un ID connu
-		echo -n "Entrer l'ID (dernière colonne) de l'album voulu"
+		echo -n $UI_albumIDinput
 		read id
 		jukebox
 		;;
@@ -334,14 +392,14 @@ function startmenu {
 		controlemplayer
 	
 	5)	# QUITTER
-	        echo "Good luck without sound !"
+	        echo $UI_quit
 		echo "stop" > /tmp/mplayer.pipe
 		rm /tmp/playlist /tmp/scnlog /tmp/lolog /tmp/mplayer.pipe 2>/dev/null
 		exit 0
 	        ;;
 	
 	*)      # mauvaise saisie
-		echo "Saisie erronée, veuillez recommencer"
+		echo $UI_wrongKey
 	        ;;
 esac
 }
